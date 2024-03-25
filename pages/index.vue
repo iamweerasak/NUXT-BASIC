934924df08169ef3759bd243e19280f977ec06c9
iamweerasak <42364854+iamweerasak@users.noreply.github.com>
2024-03-25 14:42:06 +0700

    Up Original
    Up Original to github

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container">
+        <h1> หน้าแรกของ Website</h1>
+        <img scr="image/about.png">
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+
+img {
+    width: 400px;
+    height: 400px;
+}
+</style>
---
+++ pages/products/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1> All Products</h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
